231d85d5219b22326cfa159885efbff48cc710a6
Akeem-Mckinson <akeembackup929@gmail.com>
2026-06-11 11:53:02 -0400

    Wire resource group and network module

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
+module "network" {
+  source              = "./modules/network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
