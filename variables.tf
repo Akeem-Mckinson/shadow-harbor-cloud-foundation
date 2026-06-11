038227d4bfabc64d11bb62b49f58b0a445deea8a
Akeem-Mckinson <akeembackup929@gmail.com>
2026-06-10 23:12:15 -0400

    Create Terraform variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "Azure Resource Group Name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment Environment"
+  type        = string
+}
