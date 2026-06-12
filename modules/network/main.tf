16214510e788219632003340d68a28ef9f72bd42
Akeem-Mckinson <akeembackup929@gmail.com>
2026-06-12 00:25:22 -0400

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -11,5 +11,3 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
-subnet_id = module.network.subnet_id
