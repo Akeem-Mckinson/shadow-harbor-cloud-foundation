747e959025a6292118d634133ae723bbf8ec7ac1
Akeem-Mckinson <akeembackup929@gmail.com>
2026-06-12 00:24:25 -0400

    Create network module outputs

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -11,3 +11,5 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+subnet_id = module.network.subnet_id
