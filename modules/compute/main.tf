9e7e0282fac60d9f9938d25f97f6406758ad709e
Akeem-Mckinson <akeembackup929@gmail.com>
2026-06-13 14:31:10 -0400

    Attach NIC to subnet

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -2,6 +2,12 @@ resource "azurerm_network_interface" "nic" {
   name                = "nic-shadowharbor-prod"
   location            = var.location
   resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
